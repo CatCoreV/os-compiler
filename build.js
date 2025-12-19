8de9acf348c23acc0064a10c84d30e8a5a1510aa
Żulian <102602711+zulian61@users.noreply.github.com>
2025-12-19 22:08:10 +0100

    I added 1 comma

--- build.js
+++ build.js
@@ -168,7 +168,7 @@
       "unable_stop_linux_app_on_macos": "Niemożna zatrzymać aplikacji Linux na macOS.",
       "unable_stop_macos_app_on_other_platform": "Niemożna zatrzymać aplikacji macOS na innej platformie.",
       "system_name_unsafe": "Nazwa systemu zawiera niedozwolone znaki.",
-      "quick": "Tryb szybki"
+      "quick": "Tryb szybki",
       "windows_arm64_unsupported": "Kompilacja na Windows ARM64 jest niewspierana, skompiluj dla x64 i użyj emulatora"
     }
   };
@@ -1018,5 +1018,6 @@
 
   window.addEventListener("DOMContentLoaded", openCompiler);
 })();
+
 
 
