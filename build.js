e4c349865750c0637697e5b68ac574a3fae376df
Żulian <102602711+zulian61@users.noreply.github.com>
2025-12-19 22:11:57 +0100

    I changed 1 word.

--- build.js
+++ build.js
@@ -169,7 +169,7 @@
       "unable_stop_macos_app_on_other_platform": "Niemożna zatrzymać aplikacji macOS na innej platformie.",
       "system_name_unsafe": "Nazwa systemu zawiera niedozwolone znaki.",
       "quick": "Tryb szybki",
-      "windows_arm64_unsupported": "Kompilacja na Windows ARM64 jest niewspierana, skompiluj dla x64 i użyj emulatora"
+      "windows_arm64_unsupported": "Kompilacja dla Windows ARM64 jest niewspierana, skompiluj dla x64 i użyj emulatora"
     }
   };
 
